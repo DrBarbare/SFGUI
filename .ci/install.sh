475770c080b8fd8e1ef479f197a5a4a921a8e5cb
DrBarbare <jrsmd.raynal@gmail.com>
2019-01-23 00:21:57 -0500

    Trying to refactor travis scripts

--- .ci/install.sh
+++ .ci/install.sh
@@ -18,12 +18,21 @@ then
     pyenv virtualenv 2.7.10 conan
     pyenv rehash
     pyenv activate conan
+    command_prefix=""
+else
+    docker pull ${DOCKER_IMAGE}
+    docker run -v ${PWD}:${PWD} -w ${PWD}  \
+               -u root \
+               --name "${RUNNER}" \
+               --rm -t -d ${DOCKER_IMAGE}
+    command_prefix="docker run ${RUNNER}"
 fi
 
-pip install conan --upgrade
-conan remote add -f bincrafters https://api.bintray.com/conan/bincrafters/public-conan
-conan profile new default --detect  # Generates default profile detecting GCC and sets old ABI
+"${command_prefix}" pip install conan --upgrade
+conan=(${command_prefix} conan)
+${conan[@]} remote add -f bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+${conan[@]} profile new default --detect  # Generates default profile detecting GCC and sets old ABI
 
 # Sets libcxx to C++11 ABI
-conan profile update settings.compiler.libcxx=libstdc++11 default
-conan install . --build sfml
+${conan[@]} profile update settings.compiler.libcxx=libstdc++11 default
+${conan[@]} install . || "${conan[@]}" install . --build sfml
--- .ci/run.sh
+++ .ci/run.sh
@@ -5,10 +5,19 @@ set -eux
 if [[ "$(uname -s)" == 'Darwin' ]];
 then
     generator="-GXcode"
+    cmake=("cmake")
+    need_stop=false
 else
     generator="-GNinja"
+    cmake=(docker run ${RUNNER} cmake)
+    need_stop=true
 fi
 
 build_dir="$(pwd)/build"
-cmake -H"$(pwd)" -B"${build_dir}" "${generator}" -DCMAKE_BUILD_TYPE=Release
-cmake --build "${build_dir}" --config Release
+${cmake[@]} -H"$(pwd)" -B"${build_dir}" "${generator}" -DCMAKE_BUILD_TYPE=Release
+${cmake[@]} --build "${build_dir}" --config Release
+
+if ${need_stop};
+then
+     docker stop "${RUNNER}"
+fi
