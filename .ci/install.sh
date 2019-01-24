2fa1b3b959b0e8e8e10db17d36580d68d8e1ea6c
DrBarbare <jrsmd.raynal@gmail.com>
2019-01-23 21:56:11 -0500

    Trying windows now.

--- .ci/install.sh
+++ .ci/install.sh
@@ -2,12 +2,15 @@
 
 set -ex
 
-if [[ "$(uname -s)" == 'Darwin' ]];
+command_prefix=""
+
+if [ "${MY_OS}" = "osx" ];
 then
     brew update || brew update
     brew install cmake || true
-
-    command_prefix=""
+elif [ "${MY_OS}" = "windows" ];
+then
+    choco install conan
 else
     docker pull ${DOCKER_IMAGE}
     docker run -v ${PWD}:${PWD} -w ${PWD}  \
@@ -17,14 +20,17 @@ else
     command_prefix=(docker exec ${RUNNER})
 fi
 
-${command_prefix[@]} pip install conan --upgrade
+if [ ! "${MY_OS}" = "windows" ];
+then
+    ${command_prefix[@]} pip install conan --upgrade
+fi
 
 conan=(${command_prefix[@]} conan)
 ${conan[@]} remote add -f bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 ${conan[@]} profile new default --detect  # Generates default profile detecting GCC and sets old ABI
 
 
-if [[ "$(uname -s)" == 'Darwin' ]];
+if [ "${MY_OS}" = "osx" ];
 then
     ${conan[@]} profile update settings.compiler.libcxx=libc++ default
     ${conan[@]} install . --build missing
